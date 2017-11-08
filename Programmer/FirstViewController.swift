9fddc3ac30b4ac971891d0760cf7871308fd4f21
ekramramu <ekramramu@gmail.com>
2017-11-08 15:39:49 +0600

    hello world

---
+++ Programmer/FirstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  Programmer
+//
+//  Created by Ekramul hoque on 11/4/17.
+//  Copyright © 2017 Ekramul hoque. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        print("modidy")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Programmer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Programmer
+//
+//  Created by Ekramul hoque on 11/4/17.
+//  Copyright © 2017 Ekramul hoque. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
